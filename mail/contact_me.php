2994aa27948f3b4ad30c126d948dfa1ade20c423
KingOfDog <kingofdog@web.de>
2016-02-05 14:50:59 +0100

    Changed php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,14 +11,13 @@ if(empty($_POST['name'])  		||
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'kingofdog@web.de'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "[Github-Webdesign] $name sendet eine Nachricht an dich";
+$email_subject = "[Webdesign] $name sendet eine Nachricht an dich";
 $email_body = "Eine neue Nachricht von der GitHub-Seite zu Webdesign!\n\n"."Details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@kingofdog.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: kingofdog@web.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
